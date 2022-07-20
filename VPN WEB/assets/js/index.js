d842bff8c0ef0bb594eb5b4ccdbc9c525820a69b
AfanasyHighland <79970339+AfanasyHighland@users.noreply.github.com>
2022-07-20 23:22:53 +0300

    works
    my works

---
+++ VPN WEB/assets/js/index.js
@@ -0,0 +1,10 @@
+$('.slider__track').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    // centerMode: true,
+    // centerPadding: '60px',
+  });
